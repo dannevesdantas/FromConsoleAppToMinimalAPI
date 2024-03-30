3211685d80a1b3fe7c5850a908c6732777656845
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-03-30 19:05:56 -0300

    feat: create hello world web application

--- FromConsoleAppToMinimalAPI/Program.cs
+++ FromConsoleAppToMinimalAPI/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace FromConsoleAppToMinimalAPI
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace FromConsoleAppToMinimalAPI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Iniciando o servidor web Kestrel...\n");
+
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", (HttpContext httpContext) =>
+            {
+                return "Hello, World!";
+            });
+
+            app.Run();
+
+            Console.WriteLine("\nO servidor web foi encerrado.");
+
+            // Acesse a aplicação web em http://localhost:5000
         }
     }
 }
